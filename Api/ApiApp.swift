516ca043e778323dacd23213dc0da614de510020
Rene <rene.soto01@iest.edu.mx>
2025-11-05 18:11:58 -0600

    Configuracion inicial del proyecto

---
+++ Api/ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiApp.swift
+//  Api
+//
+//  Created by MAESTRO603 on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Api/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Api
+//
+//  Created by MAESTRO603 on 05/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
